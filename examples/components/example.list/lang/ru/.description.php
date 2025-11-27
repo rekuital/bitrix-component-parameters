bf200b794259c4ff827d04e827994bf5edba48bc
Arseny <rek.a.kruglov@gmail.com>
2025-11-27 23:20:47 +0300

    Удаление лишнего, создание файла README.md

---
+++ examples/components/example.list/lang/ru/.description.php
@@ -0,0 +1,4 @@
+<?php
+$MESS['EXAMPLE_LIST_DESCRIPTION_NAME'] = 'Пример списка';
+$MESS['EXAMPLE_LIST_DESCRIPTION_DESCRIPTION'] = 'Компонент для отображения пример конфигурации параметров';
+$MESS['EXAMPLE_LIST_DESCRIPTION_GROUP'] = 'Компоненты примеров';
